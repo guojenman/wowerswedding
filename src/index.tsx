0c5915fc26f61bb3a672d6ff99df6528c2c4fc16
guojenman <guojenman@gmail.com>
2017-11-24 11:11:40 -0500

    moved to docs

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
 
 import { ComingSoon } from "./ComingSoon";
@@ -8,4 +7,3 @@ import { ComingSoon } from "./ComingSoon";
 ReactDOM.render(<ComingSoon />, document.getElementById(
   "root"
 ) as HTMLElement);
-registerServiceWorker();
